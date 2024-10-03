66860c8b0af620b85a2db0b3087f6b3f8259cb1a
Jons <ozodjon160605@gmail.com>
2024-10-03 17:51:51 +0500

    o

--- service/s-attractions.go
+++ service/s-attractions.go
@@ -76,3 +76,47 @@ func (s *TourismService) RemoveHistoricalImage(ctx context.Context, in *pb.Histo
 	}
 	return req, nil
 }
+func (s *TourismService) CreateAttractionType(ctx context.Context, in *pb.Attraction) (*pb.AttractionResponse, error) {
+	req, err := s.att.CreateAttraction(in)
+	if err != nil {
+		s.logger.Error("Error in CreateAttraction", err)
+		return nil, err
+	}
+	return req, nil
+}
+
+func (s *TourismService) UpdateAttractionType(ctx context.Context, in *pb.UpdateAttraction) (*pb.AttractionResponse, error) {
+	req, err := s.att.UpdateAttraction(in)
+	if err != nil {
+		s.logger.Error("Error in UpdateAttraction", err)
+		return nil, err
+	}
+	return req, nil
+}
+
+func (s *TourismService) GetAttractionTypeByID(ctx context.Context, in *pb.AttractionId) (*pb.AttractionResponse, error) {
+	req, err := s.att.GetAttractionByID(in)
+	if err != nil {
+		s.logger.Error("Error in GetAttractionByID", err)
+		return nil, err
+	}
+	return req, nil
+}
+
+func (s *TourismService) ListAttractionTypes(ctx context.Context, in *pb.AttractionList) (*pb.AttractionListResponse, error) {
+	req, err := s.att.ListAttractions(in)
+	if err != nil {
+		s.logger.Error("Error in ListAttractions", err)
+		return nil, err
+	}
+	return req, nil
+}
+
+func (s *TourismService) DeleteAttractionType(ctx context.Context, in *pb.AttractionId) (*pb.Message, error) {
+	req, err := s.att.DeleteAttraction(in)
+	if err != nil {
+		s.logger.Error("Error in DeleteAttraction", err)
+		return nil, err
+	}
+	return req, nil
+}
