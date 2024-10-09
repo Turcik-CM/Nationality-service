472b91c1b826cdc29f2ad2595533990e5ddb8f2d
fazliddin <hajrullaevfazliddin058@gmail.com>
2024-10-09 16:44:55 +0500

    mig fixsed

--- migrations/000001_auth_service_table.up.sql
+++ migrations/000001_auth_service_table.up.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS countries
 CREATE TABLE IF NOT EXISTS cities
 (
     id          UUID DEFAULT gen_random_uuid(),
-    country_id  UUID REFERENCES countries (id),
+    country_id  UUID REFERENCES countries (id) ON DELETE CASCADE,
     name        VARCHAR PRIMARY KEY
 );
 
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS history
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name        VARCHAR(255) NOT NULL,
     description TEXT,
-    city        VARCHAR REFERENCES cities (name),
+    city        VARCHAR REFERENCES cities (name) ON DELETE CASCADE,
     image_url   VARCHAR(255),
     created_at  TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
     updated_at  TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS attractions
     category    VARCHAR REFERENCES attraction_types (name),
     name        VARCHAR(255) NOT NULL,
     description TEXT,
-    city        VARCHAR REFERENCES cities (name),
+    city        VARCHAR REFERENCES cities (name) ON DELETE CASCADE,
     location    VARCHAR(255),
     image_url   VARCHAR(255),
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS foods
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     food_name   VARCHAR(255) NOT NULL UNIQUE,
     food_type   VARCHAR(100),
-    country_id  UUID REFERENCES countries (id),
+    country_id  UUID REFERENCES countries (id) ON DELETE CASCADE,
     description TEXT,
     ingredients TEXT,
     image_url   VARCHAR(255),
